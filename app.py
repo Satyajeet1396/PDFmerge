c77f80a27da6adf3d259aabdecbdb9433e0fbfdb
Satyajeet1396 <36727765+Satyajeet1396@users.noreply.github.com>
2024-10-29 16:13:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,16 +36,27 @@ if st.button("Create Binder") and uploaded_files:
     st.success("Binder created successfully! Click the button to download.")
 else:
     st.info("Upload PDF files and click 'Create Binder' to proceed.")
-
-# Display custom "Buy Me a Coffee" button
-bmc_button = """
-<div align="center">
-    <a href="https://www.buymeacoffee.com/researcher13" target="_blank">
-        <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Support our Research" style="height: 50px; width: 217px;">
-    </a>
-</div>
-"""
-st.markdown(bmc_button, unsafe_allow_html=True)
-
 st.info("Created by Dr. Satyajeet Patil")
 st.info("For more cool apps like this visit: https://patilsatyajeet.wixsite.com/home/python")
+
+
+# Display a "Buy Me a Coffee" button as a right-floating image link
+st.markdown(
+    """
+    <style>
+    .fixed-button {
+        position: fixed;
+        bottom: 20px;
+        right: 20px;
+    }
+    </style>
+    <div class="fixed-button">
+        <a href="https://www.buymeacoffee.com/researcher13" target="_blank">
+            <img src="https://cdn.buymeacoffee.com/widget/assets/coffee%20cup.svg" alt="Support me on Buy Me a Coffee" style="height: 50px; width: 50px;">
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
+
+
